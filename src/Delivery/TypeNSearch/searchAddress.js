be393831726f6d563b7d7b631ec4cb4ae3376acc
alvy <alvyjudy@gmail.com>
2021-01-01 20:55:30 -0500

    shorten delay

--- src/Delivery/TypeNSearch/searchAddress.js
+++ src/Delivery/TypeNSearch/searchAddress.js
@@ -1,11 +1,11 @@
 export default (addr) => {
   return new Promise((resolve, reject)=>{
     if (addr === "false") {
-      setTimeout(()=>{resolve(["Address not found"])}, 1000)
+      setTimeout(()=>{resolve(["Address not found"])}, 200)
     } else {
       setTimeout(()=>{resolve(
         [`${addr}, ON`, `${addr}, QB`, `${addr}, NS`, `${addr}, BC`, `${addr}, NL`, `${addr}, MB`]
-      )}, 1000)
+      )}, 200)
     }
   })
 }
