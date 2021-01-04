00d09d757d05c473e53f58b4a3b3e1e7e6db75e7
alvy <alvyjudy@gmail.com>
2021-01-04 04:08:40 -0500

    add a price quote

--- src/Delivery/RequestPickup/index.js
+++ src/Delivery/RequestPickup/index.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, Suspense, useRef} from "react";
 import {Provider} from "react-redux";
 
 import Request from "./Request";
+import Quote from "./Quote";
 const Map = React.lazy(()=>import("./Map"));
 import store from "./reducer";
 
@@ -12,10 +13,12 @@ const RequestPickup = () => {
     <div className={$s.RequestPickup}>
       <Provider store={store}>
         <Request data-cy="Request" />
-
+        <Quote/>
         <Suspense fallback={<div>Loading...</div>}>
           <Map className={$s.Map}/>
         </Suspense>
+
+        
 
       </Provider>
 
