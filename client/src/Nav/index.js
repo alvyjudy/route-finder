55e708d816dc7e60c3d1bde1563b9123dab0b7e1
alvy <alvyjudy@gmail.com>
2021-02-10 16:47:58 -0500

    styling

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -1,14 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import $s from "./index.scss";
 
 export default () => {
   return (
     <div className={$s.NavBar}>
-      <img className={$s.Logo} src="/assets/logo.svg" alt="parcel-connect"/>
-      <Link to="/login" className={$s.Link}>Login</Link>
-      <Link to="/signup" className={$s.Link}>Signup</Link>
+      <div className={$s.primaryNavigation}>
+        <div className={$s.LogoContainer}>
+          <img className={$s.Logo} src="/assets/logo.svg" alt="route finder" />
+        </div>
+      </div>
+
+      <div className={$s.secondaryNavigation}>
+        <Link to="/login" className={$s.Link}>
+          Login
+        </Link>
+        <Link to="/signup" className={$s.Link}>
+          Signup
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
