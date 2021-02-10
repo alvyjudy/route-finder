4b1a242e450625073a7766e56b4033c54252991d
alvy <alvyjudy@gmail.com>
2021-02-10 16:55:11 -0500

    code format, rebrand

--- client/src/Delivery/RequestPickup/Request.js
+++ client/src/Delivery/RequestPickup/Request.js
@@ -1,42 +1,43 @@
-import React, {useState, useRef} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import $s from "./Request.scss";
 import TypeNSearch from "../TypeNSearch";
-import {setLocations} from "./actions";
+import { setLocations } from "./actions";
 
 const Request = (props) => {
-  const distance = useSelector(store=>store.distance);
+  const distance = useSelector((store) => store.distance);
   const dispatch = useDispatch();
   const pickup = useRef();
   const dest = useRef();
 
-  const calculateRoute = e => {
+  const calculateRoute = (e) => {
     e.preventDefault();
     dispatch(setLocations(pickup.current, dest.current));
-  }
+  };
 
   return (
     <div className={$s.Request} data-cy="Request">
-      <p className={$s.ReqPickupTitle}>Deliver your parcel now</p>
-      <form className={$s.RequestPickupForm}
-        onSubmit={calculateRoute}
-      >
-          <TypeNSearch 
-            valueRef={pickup}
-            className={$s.SearchBox}
-            data-cy="PickupLocation" 
-            whatToType="Enter pickup location"/>
-          <TypeNSearch 
-            valueRef={dest}
-            className={$s.SearchBox} 
-            data-cy="Destination" 
-            whatToType="Enter destination"/>
-          <button type="submit" className={$s.RequestButton} data-cy="LoadMap"
-          >Request</button>
+      <p className={$s.ReqPickupTitle}>Find your way</p>
+      <form className={$s.RequestPickupForm} onSubmit={calculateRoute}>
+        <TypeNSearch
+          valueRef={pickup}
+          className={$s.SearchBox}
+          data-cy="PickupLocation"
+          whatToType="Enter starting location"
+        />
+        <TypeNSearch
+          valueRef={dest}
+          className={$s.SearchBox}
+          data-cy="Destination"
+          whatToType="Enter destination"
+        />
+        <button type="submit" className={$s.RequestButton} data-cy="LoadMap">
+          Search
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Request;
